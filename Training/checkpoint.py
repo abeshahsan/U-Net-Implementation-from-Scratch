d72c8a474f6600629823291b13b655b45ac5109c
abeshahsan <abeshahsan2002@gmail.com>
2024-07-16 02:59:28 +0600

    rename Training/utils -> Training/checkpoint

--- Training/checkpoint.py
+++ Training/checkpoint.py
@@ -4,10 +4,10 @@ def save_checkpoint(state, filename):
     print("=> Saving checkpoint")
     torch.save(state, filename) # model.state_dict() + optimizer.state_dict() + epoch
     
-def load_checkpoint(checkpoint, model, optimizer=None):
+def load_checkpoint(ckpt_file, model, optimizer=None):
+    checkpoint = torch.load(ckpt_file)
     print("=> Loading checkpoint")
     model.load_state_dict(checkpoint["state_dict"])
     if optimizer:
         optimizer.load_state_dict(checkpoint["optimizer"])
     return checkpoint["epoch"]
-
